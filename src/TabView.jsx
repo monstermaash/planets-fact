200b04acc61f93dd1508810aa263a5ecbddb867b
monstermaash <andy.vo170@hotmail.com>
2024-05-13 12:08:17 -0400

    Update button styling

--- src/TabView.jsx
+++ src/TabView.jsx
@@ -1,23 +1,50 @@
 import React, { Component } from 'react';
 import './styles/TabView.scss';
-import './styles/styles.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
 
 class TabView extends Component {
   render() {
     const { activeTab, planetInfo, onTabChange } = this.props;
+
+    if (!planetInfo) {
+      return null; // If planetInfo is not available, render nothing
+    }
+
     const { overview, structure, geology } = planetInfo;
+
+    // Determine planet color based on the selected planet
+    const planetColors = {
+      Mercury: '#b1d5e2',
+      Venus: '#f7cc7f',
+      Earth: '#545bfe',
+      Mars: '#ff6a45',
+      Jupiter: '#ecad7a',
+      Saturn: '#fccb6b',
+      Uranus: '#65f0d5',
+      Neptune: '#497efa'
+    };
+
+    const getButtonStyle = (tabName) => {
+      const isActive = activeTab === tabName;
+      const planetColor = planetColors[planetInfo.name] || '#FFFFFF';
+
+      return {
+        backgroundColor: isActive ? planetColor : 'transparent',
+        color: '#FFFFFFBF', 
+      };
+    };
 
     return (
       <div className="tab-view">
         <div className="tab-content">
+          {/* Render tab content based on the active tab */}
           {activeTab === 'overview' && (
             <>
-              <p>{overview.content}</p>
+              <p>{overview?.content}</p>
               <div className="source">
                 <span>Source: </span>
-                <a href={overview.source} target="_blank" rel="noopener noreferrer">
+                <a href={overview?.source} target="_blank" rel="noopener noreferrer">
                   Wikipedia
                   <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
                 </a>
@@ -26,10 +53,10 @@ class TabView extends Component {
           )}
           {activeTab === 'structure' && (
             <>
-              <p>{structure.content}</p>
+              <p>{structure?.content}</p>
               <div className="source">
                 <span>Source: </span>
-                <a href={structure.source} target="_blank" rel="noopener noreferrer">
+                <a href={structure?.source} target="_blank" rel="noopener noreferrer">
                   Wikipedia
                   <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
                 </a>
@@ -38,10 +65,10 @@ class TabView extends Component {
           )}
           {activeTab === 'geology' && (
             <>
-              <p>{geology.content}</p>
+              <p>{geology?.content}</p>
               <div className="source">
                 <span>Source: </span>
-                <a href={geology.source} target="_blank" rel="noopener noreferrer">
+                <a href={geology?.source} target="_blank" rel="noopener noreferrer">
                   Wikipedia
                   <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
                 </a>
@@ -50,8 +77,10 @@ class TabView extends Component {
           )}
         </div>
         <div className="tab-buttons">
+          {/* Render tab buttons with dynamic background color */}
           <button
             className={activeTab === 'overview' ? 'active' : ''}
+            style={getButtonStyle('overview')}
             onClick={() => onTabChange('overview', planetInfo.images.planet)}
           >
             <p>01</p>
@@ -59,6 +88,7 @@ class TabView extends Component {
           </button>
           <button
             className={activeTab === 'structure' ? 'active' : ''}
+            style={getButtonStyle('structure')}
             onClick={() => onTabChange('structure', planetInfo.images.internal)}
           >
             <p>02</p>
@@ -66,6 +96,7 @@ class TabView extends Component {
           </button>
           <button
             className={activeTab === 'geology' ? 'active' : ''}
+            style={getButtonStyle('geology')}
             onClick={() => onTabChange('geology', planetInfo.images.geology)}
           >
             <p>03</p>
