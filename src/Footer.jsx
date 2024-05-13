5dd5d5b7b84acb7e5a774dc3fa93416d06df7465
monstermaash <andy.vo170@hotmail.com>
2024-05-13 11:31:23 -0400

    Add animations for stats value

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import planetsData from './data/data.json';
+import anime from 'animejs'; // Import animejs for animations
 import './styles/Footer.scss';
 
 const Footer = ({ planetName }) => {
   const planetInfo = planetsData.find((planet) => planet.name === planetName);
+  const statsRefs = {
+    rotation: useRef(null),
+    revolution: useRef(null),
+    radius: useRef(null),
+    temperature: useRef(null)
+  };
+
+  useEffect(() => {
+    const animateStats = () => {
+      Object.entries(statsRefs).forEach(([statKey, ref]) => {
+        const target = ref.current;
+        if (target) {
+          const value = target.innerHTML;
+          anime({
+            targets: target,
+            innerHTML: [0, value],
+            easing: 'linear',
+            round: 1 // Will round the animated value to 1 decimal
+          });
+        }
+      });
+    };
+
+    animateStats(); // Trigger the statistics animation when the component mounts or updates
+  }, [planetName]); // Re-run the effect when planetName prop changes
 
   if (!planetInfo) {
     return null;
@@ -23,19 +49,19 @@ const Footer = ({ planetName }) => {
       <div className="footer-grid-container">
         <div className="stats">
           <p>Rotation Time</p>
-          <span>{formatStat(rotation, 'days')}</span>
+          <span ref={statsRefs.rotation}>{formatStat(rotation, 'days')}</span>
         </div>
         <div className="stats">
           <p>Revolution Time</p>
-          <span>{formatStat(revolution, 'days')}</span>
+          <span ref={statsRefs.revolution}>{formatStat(revolution, 'days')}</span>
         </div>
         <div className="stats">
           <p>Radius</p>
-          <span>{formatStat(radius, 'km')}</span>
+          <span ref={statsRefs.radius}>{formatStat(radius, 'km')}</span>
         </div>
         <div className="stats">
           <p>Average Temp.</p>
-          <span>{formatStat(temperature, '°C')}</span>
+          <span ref={statsRefs.temperature}>{formatStat(temperature, '°C')}</span>
         </div>
       </div>
     </div>
